91543b4b82c0225b6a3eec3e4811e9de42334e7f
MayaChen350 <maychennn@protonmail.com>
2024-06-12 16:14:52 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+# base files
+import discord
+from discord.ext import commands
+import settings
+# from utils import *
+
+#features
+
+def run():
+    intents = discord.Intents.all()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+################################
+# BASE                        
+################################
+    @bot.event
+    async def on_ready(): 
+        await bot.wait_until_ready()
+        await bot.tree.sync()
+        print("Bot is ready.")
+
+    @bot.event
+    async def on_command_error(ctx,error):
+        if isinstance(error,commands.MissingRequiredArgument):
+            await ctx.send("Parameters are missing.")
+        else:
+            await ctxt.send(error)
+################################
+# Commands
+################################             
+   # bot.add_command(send)
+   # bot.add_command(check_mayssages)
+################################
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+################################
+
+if __name__ == "__main__":
+    run()
---
+++ settings.py
@@ -0,0 +1,8 @@
+import pathlib
+import os
+from logging.config import dictConfig
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
